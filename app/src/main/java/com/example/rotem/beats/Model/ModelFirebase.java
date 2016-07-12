b7c0fa9ca84c99b232f7d07f8c037f1c29f17c30
rotem-ad <sj2x2djv>
2016-07-12 19:08:16 +0300

    Test db functionality

--- app/src/main/java/com/example/rotem/beats/Model/ModelFirebase.java
+++ app/src/main/java/com/example/rotem/beats/Model/ModelFirebase.java
@@ -6,11 +6,28 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by Rotem on 25/06/2016.
  */
 public class ModelFirebase {
+
+    FirebaseDatabase database;
+    DatabaseReference dbRef;
+
+    public ModelFirebase() {
+        database = FirebaseDatabase.getInstance();
+        dbRef = database.getReference();
+
+        // TODO: need to test
+        Playlist pl1 = new Playlist("123456","Rotem's Playlist","Rotem");
+        Playlist pl2 = new Playlist("123123","John's Playlist","John");
+        dbRef.child("playlists").child("id1").setValue(pl1);
+    }
+
+
     public void signup(String email, String password, final Model.AuthListener listener){
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
